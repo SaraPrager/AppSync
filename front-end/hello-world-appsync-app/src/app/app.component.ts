2ac1b4133095ed853dbbcf136a6f979165bbe1ab
Sara Prager <sara_prager@tripplite.com>
2020-10-26 22:10:35 +0200

    App structure

--- front-end/hello-world-appsync-app/src/app/app.component.ts
+++ front-end/hello-world-appsync-app/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  app_name: string = 'My App';
   source_code_link: string = 'https://github.com/SaraPrager/hello-world-appsync-app';
 }
