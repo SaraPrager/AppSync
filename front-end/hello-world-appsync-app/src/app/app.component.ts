98baf50285f76fd391eb20654eda245d321e7b72
Sara Prager <sara_prager@tripplite.com>
2020-11-02 15:22:19 +0200

    Login support

--- front-end/hello-world-appsync-app/src/app/app.component.ts
+++ front-end/hello-world-appsync-app/src/app/app.component.ts
@@ -8,10 +8,10 @@ import { Component } from '@angular/core';
 export class AppComponent {
   app_name: string = 'My App';
   source_code_link: string = 'https://github.com/SaraPrager/hello-world-appsync-app';
-  header_icons: string[] = [
-    'language',
-    'settings',
-    'login'
+  header_icons: any = [
+    { name: 'language', url: '/'},
+    { name: 'settings', url: '/'},
+    { name: 'login', url: '/login'},
   ];
   sidebar_links: any = [
     { label: 'Dashboard', url: '/'},
